3696f3ba11169958cd248a64ec16c086643f87cf
Joseph4246552 <164223131+Joseph4246552@users.noreply.github.com>
2024-03-20 10:20:32 -0700

    Add files via upload

---
+++ Silly Sentence.py
@@ -0,0 +1,8 @@
+from random import randint
+
+Nou=["Women","Paulino Marsupial","Airplane","South Korea","HIV","Brazil"]
+Ver=["Running","Driving","Bashing","Griddy-ing","Assaulting","Drinking"]
+Adj=["Collywobbles","Gullible","Colossal","Stocky","Yummy","Smart"]
+Con=["and","or","so","for","is","but"]
+print(Nou[randint(0, len(Nou)-1)], Con[randint(0,len(Con)-1)], Ver[randint(0,len(Ver)-1)],
+                                                                 Adj[randint(0,len(Adj)-1)])
